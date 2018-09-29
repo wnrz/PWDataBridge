51859f4fdbf1147c2cf7ee3aa833ff949e2bf420
王宁 <>
2018-09-29 14:58:42 +0800

    no message

---
+++ PWDataBridge/PWDataBridge/MessageCenter/PWMessageQueueDataBridge.h
@@ -0,0 +1,16 @@
+//
+//  PWBaseDataBridge.h
+//  
+//
+//  Created by 王宁 on 2018/6/12.
+//  Copyright © 2018年 xiongguobing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <PWDataBridge/PWBaseDataBridge.h>
+
+@interface PWMessageQueueDataBridge : PWBaseDataBridge
+
++ (PWMessageQueueDataBridge *)shareBridge;
+
+@end
---
+++ PWDataBridge/PWDataBridge/MessageCenter/PWMessageQueueModel.m
@@ -0,0 +1,17 @@
+//
+//  PWMessageQueueModel.m
+//  ThreadTest
+//
+//  Created by 王宁 on 2018/7/6.
+//  Copyright © 2018年 王宁. All rights reserved.
+//
+
+#import "PWMessageQueueModel.h"
+
+@implementation PWMessageQueueModel
+
+- (void)dealloc{
+    _topic = nil;
+    _param = nil;
+}
+@end
